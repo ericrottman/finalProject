d916c0e9e7acc3b0362def3140f0eae7552b1601
erott <amromrottman@gmail.com>
2020-09-25 16:52:56 -0400

    updated simple final
    simple final is fully working except for the try block for the connection

--- simple final.py
+++ simple final.py
@@ -6,6 +6,7 @@ import requests
 print('welcome to the program')
 print('we are here to help you with all your weather related needs')
 
+
 def add_more():
     again = input('Would you like to search for the weather of another city? ')
     if again in yes_list:
@@ -22,57 +23,85 @@ def add_more():
         print()
         print()
         add_more()
-def connect():
-    website = 'http://https://openweathermap.org/'
-    error_number = 0
-    try:
-        r = requests.get(website)
-    except requests.exeptions.ConnectionError:
-        print('connection unsuccessful')
-        print('retrying up to 5 times')
-        error_number += 1
-        if error_number <= 5:
-            connect()
-        elif error_number > 5:
-            print('We were unable to make a connection')
-            add_more()
 
-def weather_data():
-    connect()
-    #connection
-    #read the text
-    #save text to json
 
-def full_weather():
-    # temp = read from json data specific key for temp
+# def connect():
+#     while True:
+#
+#         try:
+#             requests.get(url)
+#         except requests.ConnectionError:
+#             error_number = int(0)
+#             print('connection unsuccessful')
+#             print('retrying up to 5 times')
+#             error_number += 1
+#             print(er)
+#             if error_number <= 5:
+#                 connect()
+#             elif error_number > 5:
+#                 print('We were unable to make a connection')
+#                 print('Please check your connection and try again')
+#                 break
+#                 add_more()
+
+def connect_2():
+    url = 'http://openweathermap.org'
+    for i in range(5):
+        try:
+            requests.get(url)
+        except requests.ConnectionError:
+            print('connection unsuccessful')
+            print('retrying up to 5 times')
+        print('Please check you connection and try again')
+        add_more()
 
 api_key = 'd8aa257457ec1e26187f1410aaa7b258'
-api_address = f'http://api.openweathermap.org/data/2.5/weather?appid={api_key}&q='
+api_address = f'api.openweathermap.org/data/2.5/weather?appid={api_key}&q='
 quit_list = ['quit', 'q', 'end', 'no', 'n']
 yes_list = ['yes', 'yeah', 'y', 'ok', 'k']
 no_list = quit_list
 help_list = ['help', 'h']
 
+
 def main():
     print('To enter a city name please type [1]. To enter a Zip code please type [2].')
     data_type = input()
+    # if data_type != str('1') or str('2'):
+    #     print('you made an invalid selection. Please try again/n')
+    #     main()
     if data_type == str('1'):
         print('What is the name of the city you would like the weather for?')
         city = input()
+        print(f'What state is {city} in? (Please use the full state name)')
+        state = input()
+        url = f'http://api.openweathermap.org/data/2.5/weather?q={city},{state}&appid={api_key}&units=imperial'
+        # connect_2()
+        json_data = requests.get(url).json()
+        temp = json_data['main']['temp']
+        temp = round(temp, 0)
+        print(f'the weather for {city.capitalize()}, {state.capitalize()} is {temp} degrees Fahrenheit')
+
+        add_more()
+
     elif data_type == str(2):
         print('What is the Zip code of the city you would like the weather for?')
         zip_code = input()
-        if len(zip_code) < 5 :
+        if len(zip_code) < 5 or len(zip_code) > 5:
             print('You did not enter a valid 5 digit zip code.')
             print('Would you like to try again? (Y or N)')
             go_back = input()
             if go_back in yes_list:
-                    main()
+                main()
             elif go_back in no_list:
                 quit()
-    connect()
-    weather_data()
-    full_weather()
-    add_more()
+        elif len(zip_code) == 5:
+            url = f'http://api.openweathermap.org/data/2.5/weather?zip={zip_code}&appid={api_key}&units=imperial'
+            # connect_2()
+            json_data = requests.get(url).json()
+            temp = json_data['main']['temp']
+            temp = round(temp, 0)
+            print(f'the weather for {zip_code} is {temp} degrees Fahrenheit')
+            add_more()
+
 
 main()
