f9d19ed8b062e743ba0f02eb1460113b4a0a608a
erott <amromrottman@gmail.com>
2020-09-25 17:00:00 -0400

    update to simple final
    added comments to allow for following of what is happening when reading the code

--- simple final.py
+++ simple final.py
@@ -2,11 +2,12 @@ import json
 import webbrowser
 import urllib
 import requests
-
+#This program is a draft
+# It pulls weather info from openweathermap.org
 print('welcome to the program')
 print('we are here to help you with all your weather related needs')
 
-
+#add more function used to allow user to loop
 def add_more():
     again = input('Would you like to search for the weather of another city? ')
     if again in yes_list:
@@ -43,7 +44,7 @@ def add_more():
 #                 print('Please check your connection and try again')
 #                 break
 #                 add_more()
-
+#still in progress....
 def connect_2():
     url = 'http://openweathermap.org'
     for i in range(5):
@@ -55,6 +56,7 @@ def connect_2():
         print('Please check you connection and try again')
         add_more()
 
+# set basic variables that will be needed for the program
 api_key = 'd8aa257457ec1e26187f1410aaa7b258'
 api_address = f'api.openweathermap.org/data/2.5/weather?appid={api_key}&q='
 quit_list = ['quit', 'q', 'end', 'no', 'n']
@@ -64,8 +66,10 @@ help_list = ['help', 'h']
 
 
 def main():
-    print('To enter a city name please type [1]. To enter a Zip code please type [2].')
-    data_type = input()
+    print('To search by City and State please type [1]')
+    print('To search by Zip code please type [2].')
+    data_type = input() #allows the user to choose what type of data they want to search by
+    #attempt to limit input to just what is wanted or give error message for incorrect input. still in progress
     # if data_type != str('1') or str('2'):
     #     print('you made an invalid selection. Please try again/n')
     #     main()
@@ -80,12 +84,12 @@ def main():
         temp = json_data['main']['temp']
         temp = round(temp, 0)
         print(f'the weather for {city.capitalize()}, {state.capitalize()} is {temp} degrees Fahrenheit')
-
         add_more()
 
     elif data_type == str(2):
         print('What is the Zip code of the city you would like the weather for?')
         zip_code = input()
+        #making sure the zip code entered is a correct length of 5 digits
         if len(zip_code) < 5 or len(zip_code) > 5:
             print('You did not enter a valid 5 digit zip code.')
             print('Would you like to try again? (Y or N)')
